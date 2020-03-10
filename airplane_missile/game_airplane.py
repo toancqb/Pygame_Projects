ea29143c1d0a7067a2dc3f073fc823279dcc6f3a
toancqb <toantqcqb@gmail.com>
2020-03-10 22:51:21 +0100

    Add Highest Score system

--- airplane_missile/game_airplane.py
+++ airplane_missile/game_airplane.py
@@ -37,15 +37,20 @@ class Player(pygame.sprite.Sprite):
         self.move_down_sound = move_down_sound
         self.surf = pygame.image.load("jet.png").convert()
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.surf.get_rect()
+        self.rect = self.surf.get_rect(
+            center = (
+                100,
+                SCREEN_HEIGHT/2,
+            )
+        )
 
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -PLAYER_SPEED)
-            self.move_up_sound.play()
+            #self.move_up_sound.play()
         if pressed_keys[K_DOWN]:
             self.rect.move_ip(0, PLAYER_SPEED)
-            self.move_down_sound.play()
+            #self.move_down_sound.play()
         if pressed_keys[K_LEFT]:
             self.rect.move_ip(-PLAYER_SPEED, 0)
         if pressed_keys[K_RIGHT]:
@@ -89,16 +94,17 @@ class Boss(pygame.sprite.Sprite):
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.surf.get_rect(
             center = (
-                random.randint(SCREEN_WIDTH/2, SCREEN_WIDTH),
+                random.randint(SCREEN_WIDTH/4, SCREEN_WIDTH),
                 0,
             )
         )
-        self.speed = random.randint(ENEMY_SPEED_MAX/2, ENEMY_SPEED_MAX)
+        self.speed = random.randint(ENEMY_SPEED_MAX/4, ENEMY_SPEED_MAX)
 
     def update(self):
         global SCORE
-        self.rect.move_ip(-self.speed, self.speed)
-        if self.rect.top > SCREEN_HEIGHT:
+        n = random.randint(0, 20)
+        self.rect.move_ip(-self.speed - n, self.speed)
+        if self.rect.top > SCREEN_HEIGHT or self.rect.right <= 0:
             SCORE = SCORE + 10
             self.kill()
 
@@ -133,108 +139,61 @@ class Game():
         pygame.mixer.music.play(loops=-1)
         # Load all sound files
         # Sound sources: Jon Fincher
-        move_up_sound = pygame.mixer.Sound("Rising_putter.ogg")
-        move_down_sound = pygame.mixer.Sound("Falling_putter.ogg")
-        collision_sound = pygame.mixer.Sound("Collision.ogg")
+        self.move_up_sound = pygame.mixer.Sound("Rising_putter.ogg")
+        self.move_down_sound = pygame.mixer.Sound("Falling_putter.ogg")
+        self.collision_sound = pygame.mixer.Sound("Collision.ogg")
 
         pygame.init()
-        font = pygame.font.SysFont("comicsansms", 40)
+        self.font = pygame.font.SysFont("comicsansms", 40)
+        self.font2 = pygame.font.SysFont("comicsansms", 72)
+
 
         self.clock = pygame.time.Clock()
 
 
         self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
-        ADDENEMY = pygame.USEREVENT + 1
-        pygame.time.set_timer(ADDENEMY, 250)
+        self.ADDENEMY = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.ADDENEMY, 250)
 
-        ADDBOSS = pygame.USEREVENT + 2
-        pygame.time.set_timer(ADDBOSS, 5000)
+        self.ADDBOSS = pygame.USEREVENT + 2
+        pygame.time.set_timer(self.ADDBOSS, 5000)
 
         self.ADDCLOUD = pygame.USEREVENT + 3
         pygame.time.set_timer(self.ADDCLOUD, 2000)
-
-        self.menu()
-
-        player = Player(move_up_sound, move_down_sound)
-        # Create groups to hold enemy sprites, cloud sprites, and all sprites
-        # - enemies is used for collision detection and position updates
-        # - clouds is used for position updates
-        # - all_sprites is used for rendering
-        enemies = pygame.sprite.Group()
-        clouds = pygame.sprite.Group()
-        all_sprites = pygame.sprite.Group()
-        all_sprites.add(player)
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        running = False
-                elif event.type == QUIT:
-                    running = False
-                elif event.type == ADDENEMY:
-                    new_enemy = Enemy()
-                    enemies.add(new_enemy)
-                    all_sprites.add(new_enemy)
-                elif event.type == ADDBOSS:
-                    new_boss = Boss()
-                    enemies.add(new_boss)
-                    all_sprites.add(new_boss)
-                elif event.type == self.ADDCLOUD:
-                    new_cloud = Cloud()
-                    clouds.add(new_cloud)
-                    all_sprites.add(new_cloud)
-
-            pressed_keys = pygame.key.get_pressed()
-
-            player.update(pressed_keys)
-            enemies.update()
-            clouds.update()
-
-            self.screen.fill(BLUE)
-
-            #screen.blit(player.surf, player.rect)
-            for entity in all_sprites:
-                self.screen.blit(entity.surf, entity.rect)
-
-            if pygame.sprite.spritecollideany(player, enemies):
-                move_up_sound.stop()
-                move_down_sound.stop()
-                collision_sound.play()
-                player.kill()
-                running = False
-
-            txt = font.render("SCORE "+str(SCORE), True, GREEN)
-            self.screen.blit(txt, (5, 5))
-            pygame.display.flip()
-            self.clock.tick(30)
-
-
-        self.game_over()
+        self.highest_score = self.get_highest_score()
+        while True:
+            global SCORE
+            self.replay = False
+            SCORE = 0
+            self.menu()
+            self.game_play()
+            self.ck_sv_highest_score()
+            self.game_over()
+            if self.replay == False:
+                break
 
         pygame.mixer.music.stop()
         pygame.mixer.quit()
         pygame.quit()
 
     def menu(self):
-        font2 = pygame.font.SysFont("comicsansms", 72)
-        font3 = pygame.font.SysFont("comicsansms", 40)
         self.screen.fill(BLUE)
 
-        txt = font2.render("-=Airplane Missile Game=-", True, RED)
+        txt = self.font2.render("-=Airplane Missile Game=-", True, RED)
         txt_center = (
             SCREEN_WIDTH/2 - txt.get_width() // 2,
             SCREEN_HEIGHT/2 - txt.get_height() // 2
         )
 
-        txt2 = font3.render("Press <Space> to Play", True, GREEN)
+        txt2 = self.font.render("Press <Space> to Play", True, GREEN)
         txt2_center = (
             SCREEN_WIDTH/2 - txt2.get_width() // 2,
             SCREEN_HEIGHT/2 + 30
         )
 
         clouds_decor = pygame.sprite.Group()
+        clouds_decor.add(Cloud())
         running = True
         while running:
             for event in pygame.event.get():
@@ -259,21 +218,86 @@ class Game():
             pygame.display.flip()
             self.clock.tick(30)
 
+    def game_play(self):
 
-    def game_over(self):
-        font2 = pygame.font.SysFont("comicsansms", 72)
-        self.screen.fill(BLUE)
-        txt = font2.render("GAME OVER! "+str(SCORE)+" SCORES!", True, RED)
-        txt_center = (
-            SCREEN_WIDTH/2 - txt.get_width() // 2,
-            SCREEN_HEIGHT/2 - txt.get_height() // 2
-        )
-        clouds_decor = pygame.sprite.Group()
+        player = Player(self.move_up_sound, self.move_down_sound)
+        # Create groups to hold enemy sprites, cloud sprites, and all sprites
+        # - enemies is used for collision detection and position updates
+        # - clouds is used for position updates
+        # - all_sprites is used for rendering
+        enemies = pygame.sprite.Group()
+        clouds = pygame.sprite.Group()
+        all_sprites = pygame.sprite.Group()
+        all_sprites.add(player)
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
+                        running = False
+                elif event.type == QUIT:
+                    running = False
+                elif event.type == self.ADDENEMY:
+                    new_enemy = Enemy()
+                    enemies.add(new_enemy)
+                    all_sprites.add(new_enemy)
+                elif event.type == self.ADDBOSS:
+                    new_boss = Boss()
+                    enemies.add(new_boss)
+                    all_sprites.add(new_boss)
+                elif event.type == self.ADDCLOUD:
+                    new_cloud = Cloud()
+                    clouds.add(new_cloud)
+                    all_sprites.add(new_cloud)
+
+            pressed_keys = pygame.key.get_pressed()
+
+            player.update(pressed_keys)
+            enemies.update()
+            clouds.update()
+
+            self.screen.fill(BLUE)
+
+            for entity in all_sprites:
+                self.screen.blit(entity.surf, entity.rect)
+
+            if pygame.sprite.spritecollideany(player, enemies):
+                self.move_up_sound.stop()
+                self.move_down_sound.stop()
+                self.collision_sound.play()
+                player.kill()
+                running = False
+
+            tmp_txt = "SCORE: ["+str(SCORE)+"]         HIGHEST SCORE OF ALL TIME: -=["+str(self.highest_score)+"]=-"
+            txt = self.font.render(tmp_txt, True, GREEN)
+            self.screen.blit(txt, (5, 5))
+            pygame.display.flip()
+            self.clock.tick(30)
+
+    def game_over(self):
+        self.screen.fill(BLUE)
+        txt = self.font2.render("GAME OVER! ["+str(SCORE)+"] SCORES!", True, RED)
+        txt_center = (
+            SCREEN_WIDTH/2 - txt.get_width() // 2,
+            SCREEN_HEIGHT/2 - txt.get_height() // 2
+        )
+
+        txt2 = self.font.render("Press <Space> to Replay", True, GREEN)
+        txt2_center = (
+            SCREEN_WIDTH/2 - txt2.get_width() // 2,
+            SCREEN_HEIGHT/2 + 30
+        )
+
+        clouds_decor = pygame.sprite.Group()
+        clouds_decor.add(Cloud())
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+                    elif event.key == K_SPACE:
+                        self.replay = True
                         running = False
                 elif event.type == QUIT:
                     running = False
@@ -285,12 +309,33 @@ class Game():
             self.screen.fill(BLUE)
 
             self.screen.blit(txt, txt_center)
-
+            self.screen.blit(txt2, txt2_center)
             for entity in clouds_decor:
                 self.screen.blit(entity.surf, entity.rect)
 
             pygame.display.flip()
             self.clock.tick(30)
+
+    def ck_sv_highest_score(self):
+        if SCORE > self.highest_score:
+            self.highest_score = SCORE
+            database = open('database.txt', 'w+')
+            database.write(str(SCORE))
+            database.close()
+
+    def get_highest_score(self):
+        try:
+            database = open('database.txt', 'r')
+        except (OSError, IOError) as e:
+            database = open('database.txt', 'w+')
+            database.write('0')
+
+        try:
+            highest_score = int(database.readline())
+        except ValueError:
+            highest_score = 0
+        database.close()
+        return highest_score
 
 
 if __name__ == '__main__':
