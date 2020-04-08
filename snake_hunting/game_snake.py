aa1029bc96bc8f1492afedf70415d189b98569a8
toancqb <toantqcqb@gmail.com>
2020-04-08 12:20:04 +0200

    Verion 1 is completed

--- snake_hunting/game_snake.py
+++ snake_hunting/game_snake.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import time
+import os.path
 from define import *
 from tools import *
 from objects_class import *
@@ -10,10 +11,25 @@ class Game():
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
+
+        self.ADDFRUIT = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.ADDFRUIT, 50000)
+
+        fruits = pygame.sprite.Group()
+        fruits.add(Fruit())
+        # snakes_sprite_group = pygame.sprite.Group()
+
         snakes = Snakes()
         snakes.add_snake()
         snakes.add_snake()
         snakes.add_snake()
+        snakes.add_snake()
+        snakes.add_snake()
+        snakes.add_snake()
+
+        wall = Wall()
+        fruits.add(wall)
+        opt = True
         running = True
         while running:
             for event in pygame.event.get():
@@ -22,13 +38,35 @@ class Game():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         running = False
+                if event.type == self.ADDFRUIT:
+                    new_fruit = Fruit()
+                    fruits.add(new_fruit)
             pressed_keys = pygame.key.get_pressed()
 
             self.draw_board(None, None, None)
+            if opt:
+                snakes.update(pressed_keys)
             for snake in snakes.group:
-                snake.update(pressed_keys)
+
+                for fruit in fruits:
+                    if pygame.sprite.collide_rect(snake, fruit):
+                        fruit.kill()
+                        new_fruit = Fruit()
+                        fruits.add(new_fruit)
+                        snakes.add_snake()
                 self.screen.blit(snake.surf, snake.rect)
 
+            for fruit in fruits:
+                self.screen.blit(fruit.surf, fruit.rect)
+            i = 1
+            while i < snakes.len:
+                if snakes.head.rect.colliderect(snakes.group[i]):
+                    expl = Explosion(snakes.head.rect.copy())
+                    opt = False
+                    break
+                i += 1
+            if not opt:
+                self.screen.blit(expl.surf, expl.rect)
             pygame.display.flip()
             self.clock.tick(GAME_SPEED)
 
@@ -37,11 +75,6 @@ class Game():
 
     def draw_board(self, lst_snake, fruit, wall):
         self.screen.fill(BLACK)
-
-        # for i in range(NX):
-        #     for j in range(NY):
-        #         pygame.draw.rect(self.screen, BLACK, (i*SQ_SIZE+1, j*SQ_SIZE+1, SQ_SIZE-2, SQ_SIZE-2))
-
 
 
 if __name__ == '__main__':
--- snake_hunting/objects_class.py
+++ snake_hunting/objects_class.py
@@ -1,19 +1,21 @@
 import pygame
 import random
+import os.path
 from define import *
 from tools import *
 
 class Snake(pygame.sprite.Sprite):
 
-    def __init__(self, rect):
+    def __init__(self, rect, index, goto):
         super(Snake, self).__init__()
         self.surf = pygame.Surface((SQ_SIZE,SQ_SIZE))
         self.surf.fill(WHITE)
         pygame.draw.rect(self.surf,GREEN, (1, 1, SQ_SIZE-2, SQ_SIZE-2))
         self.rect = self.surf.get_rect(center=(rect[0]+SQ_SIZE/2,rect[1]+SQ_SIZE/2))
-        self.goto = (SQ_SIZE, 0)
+        self.goto = goto
+        self.index = index
 
-    def update(self, pressed_keys):
+    def update2(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.goto = (0, -SQ_SIZE)
         if pressed_keys[K_DOWN]:
@@ -34,20 +36,75 @@ class Snake(pygame.sprite.Sprite):
         if self.rect.bottom > SCREEN_HEIGHT:
             self.rect.bottom = 0
 
+    def update(self):
+        self.rect.move_ip(self.goto[0],self.goto[1])
+
+        if self.rect.left < 0:
+            self.rect.left = SCREEN_WIDTH - SQ_SIZE
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = 0
+        if self.rect.top < 0:
+            self.rect.top = SCREEN_HEIGHT - SQ_SIZE
+        if self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = 0
+
 class Snakes(pygame.sprite.Sprite):
 
     def __init__(self):
         super(Snakes, self).__init__()
-        self.head = Snake((0,0))
+        self.head = Snake((0,0), 1, (SQ_SIZE, 0))
         self.group = [self.head]
+        self.len = 1
 
 
     def add_snake(self):
         rect = self.head.rect
         rect = (rect[0]+self.head.goto[0], rect[1]+self.head.goto[1], rect[2], rect[3])
-        new_snake = Snake(rect)
+        new_snake = Snake(rect, self.len, self.head.goto)
         self.group.insert(0, new_snake)
+        self.len += 1
         self.head = new_snake
 
     def update(self, pressed_keys):
-        pass
+        i = self.len-1
+        while i > 0:
+            self.group[i].goto = self.group[i-1].goto
+            i -= 1
+        self.head.update2(pressed_keys)
+        i = 1
+        while i < self.len:
+            self.group[i].update()
+            i += 1
+
+class Wall(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super(Wall, self).__init__()
+        self.surf = pygame.Surface((SQ_SIZE,SQ_SIZE*10))
+        self.surf.fill(ORANGE)
+        pygame.draw.rect(self.surf,WHITE, (2, 2, SQ_SIZE-4, SQ_SIZE-4))
+        self.rect = self.surf.get_rect(
+            center = (SCREEN_WIDTH/2,
+                      SCREEN_HEIGHT/2)
+        )
+
+
+class Fruit(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Fruit, self).__init__()
+        self.surf = pygame.Surface((SQ_SIZE,SQ_SIZE))
+        # self.surf.fill(RED)
+        self.surf.fill(ORANGE)
+        pygame.draw.rect(self.surf,RED, (2, 2, SQ_SIZE-4, SQ_SIZE-4))
+        self.rect = self.surf.get_rect(
+            center = (random.randint(0,SCREEN_WIDTH-2)+SQ_SIZE/2,
+                      random.randint(0,SCREEN_HEIGHT-2)+SQ_SIZE/2)
+        )
+
+
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, rect):
+        super(Explosion, self).__init__()
+        self.surf = pygame.image.load(os.path.join("srcs", "explosion2.png"))
+        self.surf = pygame.transform.scale(self.surf, (64, 64))
+        self.rect = rect
